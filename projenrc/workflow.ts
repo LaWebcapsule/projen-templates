f9c1e3ac9dde01d42cec9a11df67971fccd7248b
louis <louis@webcapsule.io>
2026-04-07 18:24:48 +0200

    suite

--- projenrc/workflow.ts
+++ projenrc/workflow.ts
@@ -99,7 +99,7 @@ export class Workflow extends Component {
           with: {
             commit: 'chore: update versions',
             title: 'chore: update versions',
-            version: "bash -c 'pnpm changeset version && pnpm install'",
+            version: 'bash -c "pnpm changeset version && pnpm install"',
             publish: 'pnpm changeset publish'
           },
           env: {
